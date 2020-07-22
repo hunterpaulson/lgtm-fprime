4c30483561773bf2d185a738f53c249d58469dbe
hunterpaulson <hwp1113@gmail.com>
2020-07-22 15:00:07 -0600

    removes bad traps

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -3,11 +3,6 @@
 # Exit when any command fails
 set -e
 
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-
 cd "$GITHUB_WORKSPACE"
 "$GITHUB_WORKSPACE/mk/ci/master.bash" QUICK
 "$GITHUB_WORKSPACE/mk/ci/master.bash" STATIC
